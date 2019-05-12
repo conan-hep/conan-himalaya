493d61d238cace7b984b62935fe71bab8d6bb2a4
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2019-05-12 14:30:05 +0200

    initial commit
    for Conan package recipe for Himalaya library

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+import os
+from conans import ConanFile, CMake, tools
+
+
+class HimalayaConan(ConanFile):
+    name = "Himalaya"
+    version = "2.0.1"
+    license = "GPL-3.0-only"
+    author = "Alexander Voigt"
+    url = "https://github.com/Himalaya-Library/Himalaya"
+    description = "C++ library to calculate three-loop corrections to the Higgs boson mass in the MSSM"
+    topics = ("HEP")
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    requires = ("eigen/[>=3.0]@conan/stable")
+    _source_subfolder = "Himalaya"
+
+    def source(self):
+        self.run("git clone https://github.com/Himalaya-Library/Himalaya")
+        self.run("cd Himalaya && git checkout {}".format(self.version))
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=self._source_subfolder)
+        cmake.build()
+
+    def package(self):
+        for header in ["HierarchyCalculator.hpp", "HierarchyObject.hpp",
+                       "Himalaya_interface.hpp", "version.hpp"]:
+            self.copy(header, dst="include{}himalaya".format(os.sep),
+                      src="Himalaya{}source{}include".format(os.sep, os.sep),
+                      keep_path=False)
+
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["Himalaya"]
+
