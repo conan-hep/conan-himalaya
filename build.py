14879b20b02fe98b61622116e2d0a7cf682eaad2
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2019-05-13 08:44:50 +0200

    restrict build to x86_64

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="Himalaya:shared")
+    builder = ConanMultiPackager(archs=["x86_64"])
+    builder.add_common_builds()
     builder.run()
